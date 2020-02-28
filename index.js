846a67ccdcf8527c2d0136835216d1284a393093
zymfe <1047832475@qq.com>
2020-02-28 20:10:06 +0800

    init

---
+++ index.js
@@ -0,0 +1,48 @@
+const Koa = require('koa')
+const Router = require('@koa/router')
+const static = require('koa-static')
+const app = new Koa()
+const router = new Router()
+const users = require('./data')
+const fs = require('fs')
+const vm = require('vm')
+
+app.use(static(__dirname + '/public'))
+
+const templateMap = {
+  index: '`' + fs.readFileSync('./public/index.htm').toString() + '`',
+  header: '`' + fs.readFileSync('./public/header.htm').toString() + '`',
+  content: '`' + fs.readFileSync('./public/content.htm').toString() + '`',
+  footer: '`' + fs.readFileSync('./public/footer.htm').toString() + '`'
+}
+
+const templateCache = {};
+
+const templateContext = vm.createContext({
+  include: function (name, data = {}) {
+    const template = templateCache[ name ] || createTemplate(name)
+    return template(data);
+  }
+});
+
+function createTemplate (templatePath) {
+  templateCache[ templatePath ] = vm.runInContext(
+    `(function (data) {
+          with (data) {
+              return \`${fs.readFileSync(templatePath, 'utf-8') }\`
+          }
+      })`,
+    templateContext
+  );
+
+  return templateCache[ templatePath ]
+}
+
+
+router.get('/index.html', async (ctx, next) => {
+  ctx.body = createTemplate('./public/index.htm')({ users })
+})
+
+app.use(router.routes(), router.allowedMethods)
+
+app.listen(3000)
